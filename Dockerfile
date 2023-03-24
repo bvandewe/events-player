e98fa3c9ce300a2b1530113b7ca884565e5212fa
Bruno van de Werve <bvandewe@cisco.com>
2023-03-24 23:49:12 +0100

    Clean-up and README

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.10-slim
 
-EXPOSE 8888
+EXPOSE 80
 
 # Keeps Python from generating .pyc files in the container
 ENV PYTHONDONTWRITEBYTECODE=1
@@ -17,4 +17,4 @@ COPY . /app
 RUN adduser -u 5678 --disabled-password --gecos "" appuser && chown -R appuser /app
 USER appuser
 
-CMD ["uvicorn", "api.app:app", "--host", "0.0.0.0", "--port", "8888"]
+CMD ["uvicorn", "api.app:app", "--host", "0.0.0.0", "--port", "80"]
--- api/app.py
+++ api/app.py
@@ -6,14 +6,38 @@ from fastapi.responses import StreamingResponse, HTMLResponse
 from fastapi.templating import Jinja2Templates
 from typing import AsyncGenerator
 
+
+description = """
+## Overview
+
+This micro-app serves as a test subscriber of CloudEvents and offers a web-based interface to quickly visualize the events received on its `POST /events/pub` endpoint. However, it does not persist data anywhere, so refreshing the page on the browser will reset the state.
+
+### Frontend
+
+The root URL shows a simple HTML page that automatically refreshes as new CloudEvents are received by the backend. The UI stacks events to show the most recent one on top. The filter input box allows users to search for any string in any event, and the UI will show or hide events accordingly. By clicking on any event header, users can toggle the display of its details. There is also a ToggleAll button next to the filter to expand or collapse all events.
+
+### CloudEvents
+
+This app provides an endpoint at `/events/pub` that accepts HTTP POST requests with CloudEvents payloads. To be valid, the payload must have the `Content-Type` header set to `application/cloudevents+json` and be formatted as per the CloudEvents Specifications v1.0.2 in JSON Format.
+
+When an event is received, it is added to a queue consumed by a background task that logs the events to a server-sent events (SSE) stream.
+
+### Streaming
+
+The SSE stream can be accessed at `/stream` using a browser or any SSE client. Each received event sends a JSON payload, including the event payload and a timestamp indicating when the event was received.
+"""
+
+
 templates = Jinja2Templates(directory="templates")
 
-app = FastAPI(title="CloudEvent Viewer",
+app = FastAPI(title="CloudEvents Viewer",
               version="0.1.0",
+              description=description,
               docs_url="/api/docs",
               redoc_url=None,
               openapi_url="/api/v1/oas.json"
               )
+
 
 log_queue = asyncio.Queue()
 
@@ -48,22 +72,24 @@ async def handle_event(payload: dict):
          tags=['Frontend'],
          operation_id="get_root",
          response_class=HTMLResponse)
-async def read_root(request: Request):
+async def get_ui(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.post(path="/events/pub",
           tags=['CloudEvents'],
           operation_id="post_events")
-async def post_event(payload: dict, background_tasks: BackgroundTasks, content_type: str = Header(..., alias="Content-Type"), valid_event: bool = Depends(validate_cloud_event)):
+async def handle_events(payload: dict, background_tasks: BackgroundTasks, content_type: str = Header(...), valid_event: bool = Depends(validate_cloud_event)):
     try:
         background_tasks.add_task(handle_event, payload)
-        return {"status": "ok"}
+        return {"status": "well received."}
     except Exception as e:
         await log_queue.put(f"Error handling event: {str(e)}")
         raise HTTPException(status_code=500, detail="Internal server error")
 
 
-@app.get("/logs")
-async def stream_logs():
+@app.get(path="/stream",
+         tags=['Streaming'],
+         operation_id="stream_events")
+async def stream_events():
     return StreamingResponse(generate_log_messages(), media_type="text/event-stream")
