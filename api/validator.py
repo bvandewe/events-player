0b0e2dfe5648798f9fc4bd1ee50bb70c1a2d82d7
Bruno van de Werve <bvandewe@cisco.com>
2023-04-11 16:02:05 +0200

    Fixed validator to support optional charset in Header

--- api/validator.py
+++ api/validator.py
@@ -12,10 +12,7 @@ log = logging.getLogger(__name__)
 async def validate_cloud_event(content_type: str = Header(...)):
     # TODO: add validation of the event attributes!
     valid = True
-    if content_type != "application/cloudevents+json":
-        log.info(f"Received invalid event with Content-Type: {content_type}")
-        valid = False
-    elif "application/cloudevents+json" not in content_type:
+    if "application/cloudevents+json" not in content_type:
         log.info(f"Missing 'application/cloudevents+json' in Content-Type: {content_type}")
         valid = False
 
